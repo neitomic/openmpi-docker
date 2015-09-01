8d95393a9b708a1882fe98f4facb92d26114197e
thanhtien522 <thanhtien522@gmail.com>
2015-09-01 02:34:12 +0000

    Update auto init mariadb

--- openstack-scripts/tools/auto_init_mariadb.sh
+++ openstack-scripts/tools/auto_init_mariadb.sh
@@ -4,20 +4,20 @@ set PASSWD_CUR [lindex $argv 0]
 set PASSWD_NEW [lindex $argv 1]
 
 spawn mysql_secure_installation
-expect "Enter current password for root (enter for none): "
+expect {Enter current password for root (enter for none):}
 send "${PASSWD_CUR}\r"
-expect "Set root password? [Y/n] "
+expect {Set root password? [Y/n] }
 send "Y\r"
-expect "New password: "
+expect {New password:}
 send "${PASSWD_NEW}\r"
-expect "Re-enter new password: "
+expect {Re-enter new password:}
 send "${PASSWD_NEW}\r"
-expect "Remove anonymous users? [Y/n] "
+expect {Remove anonymous users? [Y/n] }
 send "n\r"
-expect "Disallow root login remotely? [Y/n] "
+expect {Disallow root login remotely? [Y/n] }
 send "n\r"
-expect "Remove test database and access to it? [Y/n] "
+expect {Remove test database and access to it? [Y/n] }
 send "Y\r"
-expect "Reload privilege tables now? [Y/n] "
+expect {Reload privilege tables now? [Y/n] }
 send "Y\r"
 interact
