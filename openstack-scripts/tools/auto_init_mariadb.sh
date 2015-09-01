3f4a72c23cefacdd1f5bb1d1cc16b4d5f633cf8f
Nguyễn Thanh Tiến <thanhtien522@gmail.com>
2015-09-01 09:09:31 +0700

    Add expect for sql secure install

---
+++ openstack-scripts/tools/auto_init_mariadb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/expect -f
+
+set PASSWD_CUR [lindex $argv 0]
+set PASSWD_NEW [lindex $argv 1]
+
+spawn mysql_secure_installation
+expect "password"
+send "${PASSWD_CUR}\r"
+expect "Set root password? [Y/n]"
+send "Y\r"
+expect "New password:"
+send "${PASSWD_NEW}\r"
+expect "Re-enter new password:"
+send "${PASSWD_NEW}\r"
+expect "Remove anonymous users? [Y/n]"
+send "n\r"
+expect "Disallow root login remotely? [Y/n]"
+send "n\r"
+expect "Remove test database and access to it? [Y/n]"
+send "Y\r"
+expect "Reload privilege tables now? [Y/n]"
+send "Y\r"
+interact
