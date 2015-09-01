0ea1ba4ef176815066407aee4335e3d1c9677c14
Nguyễn Thanh Tiến <thanhtien522@gmail.com>
2015-09-01 09:14:28 +0700

    Update init mariadb

--- openstack-scripts/tools/auto_init_mariadb.sh
+++ openstack-scripts/tools/auto_init_mariadb.sh
@@ -4,7 +4,7 @@ set PASSWD_CUR [lindex $argv 0]
 set PASSWD_NEW [lindex $argv 1]
 
 spawn mysql_secure_installation
-expect "password"
+expect "Enter current password for root (enter for none):"
 send "${PASSWD_CUR}\r"
 expect "Set root password? [Y/n]"
 send "Y\r"
